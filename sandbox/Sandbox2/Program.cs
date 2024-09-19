5789afaffbb8b50f186b852b95347f0a2a1e0686
Dr0zax <geodashfan4@gmail.com>
2024-09-19 10:13:09 -0600

    sandbox setup practice

---
+++ sandbox/Sandbox2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Sandbox2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Howdy, Potatoes!");
+        Console.WriteLine("Whats yer name?");
+        string name = Console.ReadLine();
+    }
+}
