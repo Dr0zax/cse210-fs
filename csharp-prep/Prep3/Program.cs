95e8f62936945da38ebb6cb1e5cb16cb0f35cb10
Dr0zax <geodashfan4@gmai.com>
2024-09-27 19:43:57 -0600

    prep 3, 4, 5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rng = new Random();
+        bool playing = true;
+
+        while (playing) {
+            int guess = -1;
+            int magicNumber = rng.Next(1, 101);
+            int guesses = 0;
+
+            while (guess != magicNumber)
+            {
+                guesses++;
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+                if (guess > magicNumber) {
+                    Console.WriteLine("Lower");
+                } 
+                else if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You made {guesses} guess(es)!");
+                }
+            }
+
+            Console.Write("Play again? ");
+            string answer = Console.ReadLine();
+
+            if (answer != "yes") {
+                playing = false;
+            }
+        }
+        
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        List<int> numberList = new();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int lastInput = -1;
+
+        int sum = 0;
+        int largest = 0;
+
+        while (lastInput != 0)
+        {   
+            Console.Write("Enter a number: ");
+            lastInput = int.Parse(Console.ReadLine());
+
+            if (lastInput != 0) {
+                numberList.Add(lastInput);
+            }
+        }
+
+        foreach (int item in numberList)
+        {
+            sum += item;
+            largest = Math.Max(largest, item);
+        }
+
+        float average = ((float)sum) / numberList.Count;
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is {largest}");
     }
 }
