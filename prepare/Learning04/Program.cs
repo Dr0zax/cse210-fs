9f9a664065fc8ce9446b878140b8f6313b51440f
Dr0zax <geodashfan4@gmai.com>
2024-11-01 18:22:00 -0600

    W07 Learning

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssingment mathAssingment = new("Andrew Jeppesen", "Multiplication", "Section 7.3", "Problems 8-19");
+        WritingAssignment writingAssignment = new("Andrew Jeppesen", "European History", "The Causes of World War II");
+        // Assignment assignment = new("Andrew Jeppesen", "Multiplication");
+
+        Console.WriteLine(mathAssingment.GetSummary());
+        Console.WriteLine(mathAssingment.GetHomeworkList());
+
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
