468f498bdcb6d41085a2ffe9da685c12af801367
Dr0zax <geodashfan4@gmai.com>
2024-11-15 20:32:46 -0700

    W09 learning

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new();
+
+        shapes.Add(new Square("Green", 10));
+        shapes.Add(new Rectangle("Red", 10, 20));
+        shapes.Add(new Circle("Purple", 5));
+        
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
     }
 }
